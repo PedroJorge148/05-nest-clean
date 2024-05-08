0f8d37f6da90a17533a9ad3988248f89db3f9363
Pedro Jorge <pedrojorge2011@gmail.com>
2024-05-08 11:08:28 -0300

    refactor: correct name of last files

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,23 +1,25 @@
 import { Module } from '@nestjs/common'
 
+import { CryptographyModule } from '../cryptography/cryptography.module'
+import { DatabaseModule } from '../database/database.module'
+
 import { AuthenticateStudentUseCase } from '@/domain/forum/app/use-cases/authenticate-student'
 import { CreateQuestionUseCase } from '@/domain/forum/app/use-cases/create-question'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/app/use-cases/fetch-recent-questions'
 import { RegisterStudentUseCase } from '@/domain/forum/app/use-cases/register-student'
-import { CryptographyModule } from '../cryptography/cryptography.module'
-import { DatabaseModule } from '../database/database.module'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/app/use-cases/get-question-by-slug'
+import { EditQuestionUseCase } from '@/domain/forum/app/use-cases/edit-question'
+import { DeleteQuestionUseCase } from '@/domain/forum/app/use-cases/delete-question'
+import { AnswerQuestionUseCase } from '@/domain/forum/app/use-cases/answer-question'
+
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
 import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
-import { GetQuestionBySlugUseCase } from '@/domain/forum/app/use-cases/get-question-by-slug'
 import { EditQuestionController } from './controllers/edit-question.cotroller'
-import { EditQuestionUseCase } from '@/domain/forum/app/use-cases/edit-question'
 import { DeleteQuestionController } from './controllers/delete-question.controller'
-import { DeleteQuestionUseCase } from '@/domain/forum/app/use-cases/delete-question'
-import { AnswerQuestionController } from './controllers/asnwer-question.controller'
-import { AnswerQuestionUseCase } from '@/domain/forum/app/use-cases/answer-question'
+import { AnswerQuestionController } from './controllers/answer-question.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
