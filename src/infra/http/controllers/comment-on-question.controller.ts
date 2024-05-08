0263e11545d1740ad5c345e6717b86bcf0551cd7
Pedro Jorge <pedrojorge2011@gmail.com>
2024-05-08 15:29:48 -0300

    feat: comment on question controller

---
+++ src/infra/http/controllers/comment-on-question.controller.ts
@@ -0,0 +1,47 @@
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  HttpCode,
+  Param,
+  Post,
+} from '@nestjs/common'
+import { CurrentUser } from '@/infra/auth/current-user.decorator'
+import { UserPayload } from '@/infra/auth/jwt.strategy'
+import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { z } from 'zod'
+import { CommentOnQuestionUseCase } from '@/domain/forum/app/use-cases/comment-on-question'
+
+const commentOnQuestionBodySchema = z.object({
+  content: z.string(),
+})
+
+type CommentOnQuestionBodySchema = z.infer<typeof commentOnQuestionBodySchema>
+
+const bodyValidationPipe = new ZodValidationPipe(commentOnQuestionBodySchema)
+
+@Controller('/questions/:questionId/comments')
+export class CommentOnQuestionController {
+  constructor(private CommentOnQuestion: CommentOnQuestionUseCase) {}
+
+  @Post()
+  @HttpCode(201)
+  async handle(
+    @Body(bodyValidationPipe) body: CommentOnQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+    @Param('questionId') questionId: string,
+  ) {
+    const { content } = body
+    const { sub: userId } = user
+
+    const result = await this.CommentOnQuestion.execute({
+      content,
+      questionId,
+      authorId: userId,
+    })
+
+    if (result.isLeft()) {
+      throw new BadRequestException()
+    }
+  }
+}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,31 +3,33 @@ import { Module } from '@nestjs/common'
 import { CryptographyModule } from '../cryptography/cryptography.module'
 import { DatabaseModule } from '../database/database.module'
 
-import { AuthenticateStudentUseCase } from '@/domain/forum/app/use-cases/authenticate-student'
-import { CreateQuestionUseCase } from '@/domain/forum/app/use-cases/create-question'
-import { FetchRecentQuestionsUseCase } from '@/domain/forum/app/use-cases/fetch-recent-questions'
-import { RegisterStudentUseCase } from '@/domain/forum/app/use-cases/register-student'
-import { GetQuestionBySlugUseCase } from '@/domain/forum/app/use-cases/get-question-by-slug'
-import { EditQuestionUseCase } from '@/domain/forum/app/use-cases/edit-question'
-import { DeleteQuestionUseCase } from '@/domain/forum/app/use-cases/delete-question'
 import { AnswerQuestionUseCase } from '@/domain/forum/app/use-cases/answer-question'
-import { EditAnswerUseCase } from '@/domain/forum/app/use-cases/edit-answer'
+import { AuthenticateStudentUseCase } from '@/domain/forum/app/use-cases/authenticate-student'
+import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/app/use-cases/choose-question-best-answer'
+import { CommentOnQuestionUseCase } from '@/domain/forum/app/use-cases/comment-on-question'
+import { CreateQuestionUseCase } from '@/domain/forum/app/use-cases/create-question'
 import { DeleteAnswerUseCase } from '@/domain/forum/app/use-cases/delete-answer'
+import { DeleteQuestionUseCase } from '@/domain/forum/app/use-cases/delete-question'
+import { EditAnswerUseCase } from '@/domain/forum/app/use-cases/edit-answer'
+import { EditQuestionUseCase } from '@/domain/forum/app/use-cases/edit-question'
 import { FetchQuestionAnswersUseCase } from '@/domain/forum/app/use-cases/fetch-question-answers'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/app/use-cases/fetch-recent-questions'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/app/use-cases/get-question-by-slug'
+import { RegisterStudentUseCase } from '@/domain/forum/app/use-cases/register-student'
 
+import { AnswerQuestionController } from './controllers/answer-question.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
+import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller'
+import { CommentOnQuestionController } from './controllers/comment-on-question.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
-import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
-import { DeleteQuestionController } from './controllers/delete-question.controller'
-import { AnswerQuestionController } from './controllers/answer-question.controller'
-import { EditAnswerController } from './controllers/edit-answer.controller'
 import { DeleteAnswerController } from './controllers/delete-answer.controller'
+import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { EditAnswerController } from './controllers/edit-answer.controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { FetchQuestionAnswersController } from './controllers/fetch-question-answers.controller'
-import { ChooseQuestionBestAnswerController } from './controllers/choose-question-best-answer.controller'
-import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/app/use-cases/choose-question-best-answer'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
@@ -44,6 +46,7 @@ import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/app/use-cases/ch
     DeleteAnswerController,
     FetchQuestionAnswersController,
     ChooseQuestionBestAnswerController,
+    CommentOnQuestionController,
   ],
   providers: [
     CreateQuestionUseCase,
@@ -58,6 +61,7 @@ import { ChooseQuestionBestAnswerUseCase } from '@/domain/forum/app/use-cases/ch
     DeleteAnswerUseCase,
     FetchQuestionAnswersUseCase,
     ChooseQuestionBestAnswerUseCase,
+    CommentOnQuestionUseCase,
   ],
 })
 export class HttpModule {}
