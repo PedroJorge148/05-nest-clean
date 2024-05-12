4fd3c0d272d65fee874bf4261d64541d3cb7ed46
Pedro Jorge <pedrojorge2011@gmail.com>
2024-05-11 23:45:30 -0300

    feat: upload and create attachment on infra

--- src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-attachments-repository.ts
@@ -9,6 +9,37 @@ export class PrismaQuestionAttachmentsRepository
   implements QuestionAttachmentsRepository
 {
   constructor(private prisma: PrismaService) {}
+  async createMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return
+    }
+    /**
+     * TODO: Create cron/job para matar attachments orfãos
+     * https://app.rocketseat.com.br/h/forum/node-js/b3ccc9d0-6bd1-4250-8d4a-6cd123af6fe5
+     */
+
+    const data = PrismaQuestionAttachmentMapper.toPrismaUpdateMany(attachments)
+
+    await this.prisma.attachment.updateMany(data)
+  }
+
+  async deleteMany(attachments: QuestionAttachment[]): Promise<void> {
+    if (attachments.length === 0) {
+      return
+    }
+
+    const attachmentIds = attachments.map((attachment) =>
+      attachment.id.toString(),
+    )
+
+    await this.prisma.attachment.deleteMany({
+      where: {
+        id: {
+          in: attachmentIds,
+        },
+      },
+    })
+  }
 
   async findManyByQuestionId(
     questionId: string,
