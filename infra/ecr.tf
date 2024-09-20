6bcf53bdd82fb1811bc323e2c51f5b34cca0f195
Pedro Jorge <pedrojorge2011@gmail.com>
2024-09-20 10:29:46 -0300

    actions: ecr login step

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,12 +1,12 @@
-# resource "aws_ecr_repository" "nest-clean" {
-#   name = "nest-clean"
-#   image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "nest-clean" {
+  name = "nest-clean"
+  image_tag_mutability = "MUTABLE"
 
-#   image_scanning_configuration {
-#     scan_on_push = true
-#   }
+  image_scanning_configuration {
+    scan_on_push = true
+  }
 
-#   tags = {
-#     Iac = "True"
-#   }
-# }
+  tags = {
+    Iac = "True"
+  }
+}
--- infra/iam.tf
+++ infra/iam.tf
@@ -40,6 +40,30 @@ resource "aws_iam_role" "ecr-role" {
     ]
   })
 
+  inline_policy {
+    name = "ecr-app-permissions"
+    policy = jsonencode({
+      Statement = [
+        {
+          Sid = "Statement1"
+          Action = [
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:BatchGetImage",
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:PutImage",
+            "ecr:InitiateLayerUpload",
+            "ecr:UploadLayerPart",
+            "ecr:CompleteLayerUpload",
+            "ecr:GetAuthorizationToken",
+          ]
+          Effect   = "Allow"
+          Resource = "*"
+        }
+      ]
+    })
+  }
+
+
   tags = {
     Iac = "True"
   }
@@ -48,6 +72,6 @@ resource "aws_iam_role" "ecr-role" {
 }
 
 output "ecr-role-arn" {
-  value = aws_iam_role.ecr-role.arn
+  value     = aws_iam_role.ecr-role.arn
   sensitive = false
 }
